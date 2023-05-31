fe032b2e61cfcc946ce8a333331d6f89ef2d5596
leander lxl <leanderlxl@gmail.com>
2023-05-31 23:13:12 +0800

    finishing  the core

--- core/mapobject.cpp
+++ core/mapobject.cpp
@@ -1,4 +1,30 @@
 #include "mapobject.h"
+#include<QDebug>
+layerManager* layerManager::singleton = nullptr;
+
+layerManager* layerManager::makeLayerManager()
+{
+    if(this->singleton == nullptr)
+    {
+        this->singleton = new layerManager();
+        return this->singleton;
+    }
+    else
+    {
+        qDebug()<<"don't fucking do this";
+        return this->singleton;
+    }
+}
+
+void Layer::setZvalue(int z)
+{
+   for(int i = 0;i<this->container.size();++i)
+   {
+       auto tmp = container[1];
+       tmp->acceptDrops();
+       tmp->setZValue(z);
+   }
+}
 
 mapObject::mapObject()
 {
--- core/mapobject.h
+++ core/mapobject.h
@@ -1,8 +1,64 @@
 #ifndef MAPOBJECT_H
 #define MAPOBJECT_H
+#include<QString>
+#include<QVector>
+#include<QGraphicsItem>
+#include<QQueue>
+//avoid polution to the outter namespace
+//the namespace of core including needed enumeration for this part
+namespace coreEnum {
+    enum layerType
+    {
+        point,
+        multiPoint,
+        polyLine,
+        polyGon,
+        null
+    };
+}
+class Layer;
+class layerManager
+{
+    static layerManager* makeLayerManager();
+    QQueue<mapObject*> getData();
+    void setLayerOrder(QQueue<QString> layerOrder);
 
+private:
+    QVector<Layer> layers;
+    static layerManager* singleton;
+};
 
-class mapObject
+//forward declration
+class mapObject;
+class Layer
+{
+public:
+    Layer(coreEnum::layerType type,QString layerName,QVector<mapObject*> data)
+    {
+        this->type = type;
+        this->layerName = layerName;
+        this->container = data;
+    }
+public:
+    //set every zValue of layerItem
+    void setZvalue(int z);
+    int getZvalue(){return zValue;}
+
+    //setter for layername
+    void setLayerName(QString name){layerName = name;}
+    QString getLayerName(){return   layerName;}
+
+    QQueue<mapObject*> getData();
+
+private:
+    int zValue = 0;
+    QString layerName = "undefined";
+    coreEnum::layerType type = coreEnum::null;
+    QVector<mapObject*> container;
+
+};
+
+class mapObject:public QGraphicsItem
 {
 public:
     mapObject();
